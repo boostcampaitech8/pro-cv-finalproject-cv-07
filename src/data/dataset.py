e0aabeeee4fdbaffb9a46a86df6fd44e1b38ecb1
wooqi00 <whddnr0905@gmail.com>
2026-01-28 09:41:03 +0000

    [Add] DeepAR 모델 추가

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,6 +1,7 @@
 import numpy as np
 import json
-
+from gluonts.dataset.pandas import PandasDataset
+import pandas as pd
 
 def build_dataset(time_series, seq_length, horizons=[1, 5, 10, 20]):
     dataX = []
@@ -49,3 +50,72 @@ def test_split(X, Y, T, split_file):
     testX, testY = X[np.isin(T, test_dates)], Y[np.isin(T, test_dates)]
     
     return test_dates, testX, testY
+
+
+
+def deepar_split(df, split_file, fold_idx):
+    with open(split_file, "r") as f:
+        data = json.load(f)
+
+    train_dates = set(str(d)[:10] for d in data["folds"][fold_idx]["train"]["t_dates"])
+    val_dates   = set(str(d)[:10] for d in data["folds"][fold_idx]["val"]["t_dates"])
+
+    df = df.copy()
+    df["date_str"] = df["time"].astype(str).str[:10]
+
+    train_df = df[df["date_str"].isin(train_dates)].drop(columns="date_str")
+    val_df   = df[df["date_str"].isin(val_dates)].drop(columns="date_str")
+    
+    train_df = (
+        train_df
+        .sort_values(["item_id", "time"])
+        .reset_index(drop=True)
+    )
+    train_df["time"] = train_df.groupby("item_id").cumcount()
+
+    val_df = (
+        val_df
+        .sort_values(["item_id", "time"])
+        .reset_index(drop=True)
+    )
+    val_df["time"] = val_df.groupby("item_id").cumcount()
+
+    return train_df, val_df
+
+
+
+
+def build_multi_item_dataset(dfs, target_col, feature_cols):
+    all_dfs = []
+    min_len = min(len(df) for df in dfs.values())
+    for item_id, df in dfs.items():
+        df = df.copy()
+        #df = df.iloc[-min_len:]
+
+        # 필수 컬럼 체크
+        assert "item_id" in df.columns, f"{item_id}: item_id missing"
+        assert target_col in df.columns, f"{item_id}: target missing"
+
+    
+        # 필요한 컬럼만 선택
+        keep_cols = ["time", "item_id", target_col] + feature_cols
+        df = df[keep_cols]
+
+        all_dfs.append(df)
+        print(f"Item: {item_id}, Target: {len(df.values)}")
+
+    # multi-item concat
+    long_df = pd.concat(all_dfs, ignore_index=True)
+
+    # PandasDataset 생성
+    dataset = PandasDataset.from_long_dataframe(
+        long_df,
+        item_id="item_id",
+        timestamp="time",
+        target=target_col,
+        freq=None,   # ← 숫자 index일 때는 None이 안전
+        feat_dynamic_real=feature_cols,
+    )
+    
+
+    return dataset
